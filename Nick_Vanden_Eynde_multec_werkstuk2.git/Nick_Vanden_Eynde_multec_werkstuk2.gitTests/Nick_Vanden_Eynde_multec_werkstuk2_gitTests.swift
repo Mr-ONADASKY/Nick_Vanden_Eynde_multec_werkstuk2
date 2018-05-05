8e7de5fa08638dced965b20ff8d1b6a2e7df1877
Nick VDE <ninjawulf98@gmail.com>
2018-05-05 17:32:21 +0200

    initiate

---
+++ Nick_Vanden_Eynde_multec_werkstuk2.git/Nick_Vanden_Eynde_multec_werkstuk2.gitTests/Nick_Vanden_Eynde_multec_werkstuk2_gitTests.swift
@@ -0,0 +1,36 @@
+//
+//  Nick_Vanden_Eynde_multec_werkstuk2_gitTests.swift
+//  Nick_Vanden_Eynde_multec_werkstuk2.gitTests
+//
+//  Created by Nickvde on 05/05/2018.
+//  Copyright © 2018 Nickvde. All rights reserved.
+//
+
+import XCTest
+@testable import https___github_com_ninjawulf98_Nick_Vanden_Eynde_multec_werkstuk2_git
+
+class Nick_Vanden_Eynde_multec_werkstuk2_gitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
